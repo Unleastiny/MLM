ed89357e9539ccf4835676667a4fbe71fa144ed6
Unleastiny <romsroms57@hotmail.fr>
2017-01-12 09:16:47 +0100

    Add files via upload

---
+++ affichage-abonne.php
@@ -0,0 +1,50 @@
+<?php
+	class Affichage_Abonne
+	{
+		public function __construct()
+		{
+			add_action('admin_menu',array($this,'addAdminMenu'),20);
+		}
+		public function addAdminMenu()
+		{
+			add_submenu_page('zero', 'Abonnés', 'Abonnés', 'manage_options', 'abonne', array($this, 'menu_html'));
+		}
+		
+		public function menu_html()
+		{
+			echo '<h1>'.get_admin_page_title().'</h1>';
+			echo '<h2>La liste des Abonnés uniquement </h2>';
+			global $wpdb;
+			$fetch = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}usager WHERE role='Abonne'", ARRAY_A);
+			echo "<table border='1'>
+					<tr>
+					<th>Nom</th>
+					<th>Prenom</th>
+					<th>Adresse Mail</th>
+					<th>RIB</th>
+					<th>login</th>
+					<th>Id du parrain</th>
+					</tr>";
+			
+			foreach($fetch as $row)
+			{
+				$nom = $row['nom'];
+				$prenom = $row['prenom'];
+				$email = $row['email'];
+				$rib = $row['rib'];
+				$login = $row['login'];
+				$parrain = $row['id_parrain'];
+				
+				echo "<tr>
+						<td>".$nom."</td>
+						<td>".$prenom."</td>
+						<td>".$email."</td>
+						<td>".$rib."</td>
+						<td>".$login."</td>
+						<td>".$parrain."</td>
+					</tr>";
+			}			
+			echo "</table>";
+		}
+	}
+?>
--- pluginMaster.php
+++ pluginMaster.php
@@ -1,17 +1,65 @@
 <?php
-/*
-Plugin Name: Formulaire d'Inscription
-Description: Le formulaire d'inscription au site MLM
-Version: 0.1
-Author: Stortz Romain
-*/
-class Formulaire_Plugin
-{
-    public function __construct()
-    {
-        include_once 'C:\wamp64\www\wordpress\wp-content\plugins\formulaire-inscription\formulaire.php';
-        new Formulaire();
-    }
-}
-new Formulaire_Plugin();
+	/*
+	Plugin Name: Listing des clients ( Abonnés / Distributeurs )
+	Description: Affiche la liste des clients qu'ils soient distributeurs ou abonnés !
+	Version: 0.1
+	Author: Stortz Romain
+	*/
+	class Affichage_Client_plugin
+	{
+		public function __construct()
+		{
+			include_once 'C:\wamp64\www\wordpress\wp-content\plugins\liste-clients\affichage-distributeur.php';
+			new Affichage_Distributeur();
+			include_once 'C:\wamp64\www\wordpress\wp-content\plugins\liste-clients\affichage-abonne.php';
+			new Affichage_Abonne();
+			add_action('admin_menu',array($this,'addAdminMenu'));
+		}
+		public function addAdminMenu()
+		{
+			add_menu_page('Listes des clients', 'Management des Usagers', 'manage_options', 'zero', array($this, 'menu_html'));
+			add_submenu_page('zero', 'Usagers', 'Usagers', 'manage_options', 'zero', array($this, 'menu_html'));
+		}
+		
+		public function menu_html()
+		{
+			echo '<h1>'.get_admin_page_title().'</h1>';
+			echo '<p>La liste des usagers ( Abonnés et Distributeurs ) </p>';
+			global $wpdb;
+			$fetch = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}usager ", ARRAY_A);
+			echo "<table border='1'>
+					<tr>
+					<th>Nom</th>
+					<th>Prenom</th>
+					<th>Adresse Mail</th>
+					<th>RIB</th>
+					<th>login</th>
+					<th>Role</th>
+					<th>Id du parrain</th>
+					</tr>";
+			
+			foreach($fetch as $row)
+			{
+				$nom = $row['nom'];
+				$prenom = $row['prenom'];
+				$email = $row['email'];
+				$rib = $row['rib'];
+				$login = $row['login'];
+				$role = $row['role'];
+				$parrain = $row['id_parrain'];
+				
+				echo "<tr>
+						<td>".$nom."</td>
+						<td>".$prenom."</td>
+						<td>".$email."</td>
+						<td>".$rib."</td>
+						<td>".$login."</td>
+						<td>".$role."</td>
+						<td>".$parrain."</td>
+					</tr>";
+			}			
+			echo "</table>";
+		}
+	}
+	new Affichage_Client_plugin();
 ?>
